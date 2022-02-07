4807afb078cdf980e488b39171e36b72d9fea04a
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-02-07 17:25:48 +0700

    fix : validation pagination

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -117,3 +117,29 @@ exports.validationStatus = (status) => {
     }
     return result;
 };
+
+exports.validationSearch = (search) => {
+    var result = null;
+    if (search == null || search == '') {
+        result = { search: 'Search must be filled.' };
+    }
+    return result;
+};
+
+exports.validationPagination = (pagination) => {
+    var result = null;
+    const { page, limit } = pagination;
+
+    if (isNaN(parseInt(page))) {
+        result = {...result, page: 'Page must be a number.' };
+    } else if (page == 0) {
+        result = {...result, page: 'Page must be grather then 0.' };
+    }
+
+    if (isNaN(parseInt(limit))) {
+        result = {...result, limit: 'Limit must be a number.' };
+    } else if (limit == 0) {
+        result = {...result, limit: 'Limit must be grather than 0.' };
+    }
+    return result;
+};
