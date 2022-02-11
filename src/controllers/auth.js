dd482f6daf8903f3b9d47a6dd6556897897598ac
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-02-11 17:09:43 +0700

    feat : add function for register

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,41 +2,69 @@ const userModel = require('../models/users');
 const argon = require('argon2');
 const jwt = require('jsonwebtoken');
 const showApi = require('../helpers/showApi');
+const validationDataUser = require('../helpers/validation');
 const { APP_SECRET } = process.env;
+const verifyUser = require("../helpers/auth");
 
-exports.login = async(req, res) => {
-    const { email, password } = req.body;
-    const result = await userModel.getDataUserEmailAsync(email, null);
-    // console.log(result);
-    if (result.length == 1) {
-        const { password: hashPassword } = result[0];
-        const checkPassword = await argon.verify(hashPassword, password);
-        if (checkPassword) {
-            const data = { id: result[0].id };
-            const token = jwt.sign(data, APP_SECRET);
-            return showApi.showResponse(res, "Login Success!", { token });
+const login = async(req, res) => {
+    try {
+        const { email, password } = req.body;
+        const result = await userModel.getDataUserEmailAsync(email, null);
+        // console.log(result);
+        if (result.length == 1) {
+            const { password: hashPassword } = result[0];
+            const checkPassword = await argon.verify(hashPassword, password);
+
+            if (checkPassword) {
+                // const data = result[0];
+                const data = { id: result[0].id };
+                const token = jwt.sign(data, APP_SECRET);
+                return showApi.showResponse(res, "Login Success!", { token });
+            } else {
+                return showApi.showResponse(res, "Wrong email or password!", null, 400);
+            }
+
         } else {
-            return showApi.showResponse(res, "Wrong email or password!", null, 400);
+            return showApi.showResponse(res, "Wrong email and password!", null, 400);
+        }
+    } catch (err) {
+        return showApi.showResponse(res, err.message, null, 400);
+    }
+
+};
+
+const register = async(req, res) => {
+    const { fullName, username, email, password } = req.body;
+    const data = { fullName, username, email, password };
+    // console.log(data);
+    var errValidation = await validationDataUser.validationRegister(data);
+    req.status = "Register";
+    if (errValidation == null) {
+        const hashPassword = await argon.hash(data.password);
+        data.password = hashPassword;
+        const resultRegister = await userModel.insertDataUserAsync(data);
+        if (resultRegister.affectedRows > 0) {
+            const data = { id: resultRegister.insertId };
+            const token = jwt.sign(data, APP_SECRET);
+            if (verifyUser.verifyUser(req, res, null)) {
+                const updateVerifyUser = await userModel.updateDataUserAsync(resultRegister.insertId, { isVerified: 1 });
+                try {
+                    if (updateVerifyUser.affectedRows > 0) {
+                        return showApi.showResponse(res, "Registration Success!", { token });
+                    } else {
+                        return showApi.showResponse(res, "Registration failed!", null, 500);
+                    }
+                } catch (err) {
+                    return showApi.showResponse(res, err.message, null, 500);
+                }
+
+            } else {
+                return showApi.showResponse(res, "Data user verify!", null);
+            }
         }
     } else {
-        return showApi.showResponse(res, "Wrong email and password!", null, 400);
+        return showApi.showResponse(res, "Data Register not valid.", errValidation, 400);
     }
 };
 
-// exports.verify = (req, res) => {
-//     const auth = req.headers.authorization;
-//     if (auth.startsWith("Bearer")) {
-//         const token = auth.split(' ')[1];
-//         if (token) {
-//             try {
-//                 if (jwt.verify(token, APP_SECRET)) {
-//                     return showApi.showResponse(res, 'User Verified');
-//                 } else {
-//                     return showApi.showResponse('User not verified!', null, 403);
-//                 }
-//             } catch (err) {
-//                 return showApi.showResponse('User not verified!', null, 403);
-//             }
-//         }
-//     }
-// };
+module.exports = { login, register };
