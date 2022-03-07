71dea5c725494b7a204f0dede792c4ad02de4694
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-03-07 09:43:16 +0700

    fix : update query in profile model

--- src/models/profile.js
+++ src/models/profile.js
@@ -1,7 +1,7 @@
 const db = require('../helpers/database');
 
 exports.getDataProfile = (id, cb) => {
-    db.query('select fullName,NickName,gender,photo,address,birthDate,mobileNumber,email from users where id=?', [id], (error, result) => {
+    db.query('select id,fullName,NickName,gender,photo,address,birthDate,mobileNumber,email from users where id=?', [id], (error, result) => {
         if (error) throw error;
         cb(result);
     });
