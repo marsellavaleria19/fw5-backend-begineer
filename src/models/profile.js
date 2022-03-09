6dfd1f84127c373e9a5456902bc862cd50d3e4d8
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-03-09 11:20:54 +0700

    fix : fix query profile

--- src/models/profile.js
+++ src/models/profile.js
@@ -1,7 +1,8 @@
 const db = require('../helpers/database');
+const { APP_URL } = process.env;
 
 exports.getDataProfile = (id, cb) => {
-    db.query('select id,fullName,NickName,gender,photo,address,birthDate,mobileNumber,email from users where id=?', [id], (error, result) => {
+    db.query(`select id,fullName,NickName,gender,concat('${APP_URL}/',photo) as photo,address,birthDate,mobileNumber,email from users where id=?`, [id], (error, result) => {
         if (error) throw error;
         cb(result);
     });
