2e767b724ae19323032eed82fec42fd0207f7ab7
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-19 04:19:59 +0700

    feat : add function upload to cloudinary

--- src/helpers/upload.js
+++ src/helpers/upload.js
@@ -1,14 +1,41 @@
+/* eslint-disable no-unused-vars */
+const cloudinary = require('cloudinary').v2;
+const {CloudinaryStorage} = require('multer-storage-cloudinary');
 const multer = require('multer');
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function(req, file, cb) {
-        const extension = file.originalname.substring(3);
-        console.log(extension);
-        cb(null, file.originalname);
-    }
+
+const { CLOUD_NAME,CLOUD_API_KEY,CLOUD_API_SECRET,ENVIRONMENT } = process.env;
+
+cloudinary.config({ 
+    cloud_name: CLOUD_NAME, 
+    api_key: CLOUD_API_KEY, 
+    api_secret: CLOUD_API_SECRET 
 });
+
+var storage = null;
+if(ENVIRONMENT=="production"){
+    storage = new CloudinaryStorage({
+        cloudinary : cloudinary,
+        params : {
+            folder:'vehicle-rent/uploads',
+            format : async (req, file)=>'png',
+            public_id : (req, file)=>{
+                const timestamp = Date.now();
+                return `${req.fileUpload}-${timestamp}`;
+            }
+        },
+    });
+}else{
+    const storage = multer.diskStorage({
+        destination: function(req, file, cb) {
+            cb(null, 'uploads/');
+        },
+        filename: function(req, file, cb) {
+            const extension = file.originalname.substring(3);
+            console.log(extension);
+            cb(null, file.originalname);
+        }
+    });
+}
 
 const fileFilter = (req, file, cb) => {
     const typeImage = [
@@ -23,6 +50,7 @@ const fileFilter = (req, file, cb) => {
         cb(null, true);
     }
 };
+
 const upload = multer({ storage: storage, fileFilter, limits: { fileSize: 2000000 } });
 
 module.exports = upload;
