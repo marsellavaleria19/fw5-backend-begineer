eccd833d7fcd439d6aae5a8575a1fa91c1de118b
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-19 04:11:48 +0700

    fix : fix route categories

--- src/routers/categories.js
+++ src/routers/categories.js
@@ -1,11 +1,16 @@
 const categories = require('express').Router();
-const { verifyUser } = require('../helpers/auth');
+const { verifyAdmin } = require('../helpers/auth');
 
-const { getCategories, getCategory, insertCategory, updateCategory, deleteCategory } = require('../controllers/categories');
-categories.get('/', getCategories);
-categories.get('/:id', getCategory);
-categories.post('/', verifyUser, insertCategory);
-categories.put('/:id', verifyUser, updateCategory);
-categories.delete('/:id', verifyUser, deleteCategory);
+const { getCategoriesAsync, getCategoryAsync, insertCategoryAsync, updateCategoryAsync, deleteCategoryAsync } = require('../controllers/categories');
+// categories.get('/', getCategories);
+categories.get('/',getCategoriesAsync);
+// categories.get('/:id', getCategory);
+categories.get('/:id',getCategoryAsync);
+// categories.post('/', verifyUser, insertCategory);
+categories.post('/', verifyAdmin, insertCategoryAsync);
+// categories.put('/:id', verifyUser, updateCategory);
+categories.put('/:id', verifyAdmin, updateCategoryAsync);
+// categories.delete('/:id', verifyUser, deleteCategory);
+categories.delete('/:id', verifyAdmin, deleteCategoryAsync);
 
 module.exports = categories;
