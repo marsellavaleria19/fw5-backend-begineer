b82293e50d28b97c9c3676c01d5d5dce8d70b748
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-19 15:46:49 +0700

    fix : change createConnection to createPool in connection database.

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,13 +1,11 @@
 const mysql = require('mysql');
 
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
-const conn = mysql.createConnection({
+const conn = mysql.createPool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME
 });
-
-conn.connect();
 
 module.exports = conn;
