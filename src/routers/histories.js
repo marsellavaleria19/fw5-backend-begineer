3e5cd3f03daf12924c97c2456d2c7c6b8746e486
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-28 17:34:39 +0700

    feat : update history route

--- src/routers/histories.js
+++ src/routers/histories.js
@@ -1,5 +1,5 @@
 const histories = require('express').Router();
-const { verifyUser,verifyAdmin } = require('../helpers/auth');
+const { verifyUser,verifyAdmin,verifyAll } = require('../helpers/auth');
 const {
     //  getHistories,
     getHistoriesAsync,
@@ -23,9 +23,9 @@ histories.get('/:id', getHistoryAsync);
 // histories.post('/', insertHistory);
 histories.post('/', verifyUser, insertHistoryAsync);
 // histories.put('/:id', updateHistory);
-histories.put('/:id', verifyAdmin, updateHistoryAsync);
-histories.patch('/:id', verifyAdmin, updatePatchHistoryAsync);
+histories.put('/:id', verifyAll, updateHistoryAsync);
+histories.patch('/:id', verifyAll, updatePatchHistoryAsync);
 // histories.delete('/:id', verifyUser, deleteHistory);
-histories.delete('/:id', verifyAdmin, deleteHistoryAsync);
+histories.delete('/:id', verifyAll, deleteHistoryAsync);
 
 module.exports = histories;
