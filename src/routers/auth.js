21cb7c2cddbc65dd72ac9ffdc3696d0665eea4e1
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-04-28 17:32:37 +0700

    feat : add change password router

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,9 +1,13 @@
 const auth = require('express').Router();
-const { login, register,refreshForToken, forgotPassword, emailVerification } = require('../controllers/auth');
+const {verifyAll} = require('../helpers/auth');
+
+const { login, register,refreshForToken, forgotPassword, emailVerification,changePassword } = require('../controllers/auth');
 
 auth.post('/login', login);
 auth.post('/refresh', refreshForToken);
 auth.post('/register', register);
 auth.post('/emailverification', emailVerification);
 auth.post('/forgotpassword', forgotPassword);
+auth.post('/changepassword',verifyAll,changePassword);
+
 module.exports = auth;
